8696bd07230cf866b812b78e692d408db089003f
SteWootten <stewootten@gmail.com>
2014-07-06 13:48:36 +0100

    Simple generation of Bootstrap tabs from JSON returned by server
    - Bootstrap tabs are generated depending on the information retrieved from
      the server

---
+++ somePHP.php
@@ -0,0 +1,23 @@
+<?php
+
+	echo '{
+			  "CAT Team": [
+			    {"TestName": "Foo", "TestDescription" : "Bar"},
+			    {"TestName": "Joe", "TestDescription" : "Blogs"},
+			    {"TestName": "Rick", "TestDescription" : "Jones"}
+			  ],
+
+			  "BT dot com": [
+			    {"TestName": "Lemon", "TestDescription" : "Cello"},
+			    {"TestName": "Adam", "TestDescription" : "Chris"},
+			    {"TestName": "Lemon", "TestDescription" : "Nanni"},
+			    {"TestName": "Adam", "TestDescription" : "Chris"},
+			    {"TestName": "Lemon", "TestDescription" : "Nanni"}
+			  ],
+
+			  "Sara Tab": [
+			    {"TestName": "Lemon", "TestDescription" : "Cello"},
+			    {"TestName": "Adam", "TestDescription" : "Chris"}
+			  ]
+		}';
+?>
